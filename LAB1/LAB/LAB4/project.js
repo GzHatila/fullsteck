6b29aa14b7ff4badb592d724bb94c9e528e38a4a
GzHatila <hatila07112006@gmail.com>
2025-03-25 17:02:30 -0300

    .

--- LAB1/LAB/LAB4/project.js
+++ LAB1/LAB/LAB4/project.js
@@ -6,16 +6,19 @@ function ex1(){
     let n1 = parseInt(document.getElementById('num1').value)
     if(n1 < aleatorio){
         result=n1
-        document.getElementById('errada').innerHTML="Menor do que esperado"
+        document.getElementById(ID_DO_ELEMENTO).innerHTML="Menor do que esperado"
+        document.getElementById('ID_DO_ELEMENTO').style.setProperty("background-color", "red");
         document.getElementById('menor').innerHTML=n1
     }
 else if(n1 > aleatorio){
        result=n1
-       document.getElementById('errada').innerHTML="Maior do que o esperado"
+       document.getElementById('ID_DO_ELEMENTO').innerHTML="Maior do que o esperado"
+       document.getElementById('ID_DO_ELEMENTO').style.setProperty("background-color", "red");
        document.getElementById('maior').innerHTML=n1
     }
     else {
         result=n1
-         document.getElementById('errada').innerHTML="Numero Coreto!!"
+         document.getElementById('ID_DO_ELEMENTO').innerHTML="Numero Coreto!!"
+         document.getElementById('ID_DO_ELEMENTO').style.setProperty("background-color", "red");
     }
 }
