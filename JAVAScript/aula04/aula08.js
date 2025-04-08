08f559b2176fe8694342a9d2992cd4a7d58fc9db
GzHatila <hatila07112006@gmail.com>
2025-04-08 16:14:27 -0300

    .

--- JAVAScript/aula04/aula08.js
+++ JAVAScript/aula04/aula08.js
@@ -73,16 +73,48 @@ let canvas1 = document.getElementById('canvas1');
 let ctx1 = canvas1.getContext('2d');
 
 let retangulo1 = new retangulo('blue','red',3,0,0,20,20);
+let retangulo2 = new retangulo('blue','green', 3 , 200,200,20,20);
+let retangulo3 = new retangulo('blue','blue', 3 , 100,100,20,20);
 retangulo1.desenhe(ctx1)
+retangulo2.desenhe(ctx1)
+retangulo3.desenhe(ctx1)
 //retangulo1.x = 100;
 //retangulo1.desenhe(ctx1)
 
 function animacao(){
+    if(retangulo1.x == 400){
+        retangulo1.x =0;
+    }
     retangulo1.x +=1;
     ctx1.clearRect(0,0,400,400)
     retangulo1.desenhe(ctx1)
+    retangulo2.desenhe(ctx1)
+    retangulo3.desenhe(ctx1)
 
     requestAnimationFrame(animacao)
 }
 
-animacao(ctx1)
+animacao()
+
+document.addEventListener('keydown', function(evento){
+    let tecla = evento.key;
+    console.log(tecla);
+
+    let velocidade = 15;
+    if(tecla == 'ArrowUp'){ retangulo2.y -= velocidade}
+    if(tecla == 'ArrowDown'){ retangulo2.y += velocidade}
+    if(tecla == 'ArrowLeft'){ retangulo2.x -= velocidade}
+    if(tecla == 'ArrowRight'){ retangulo2.x += velocidade}
+})
+
+document.addEventListener('mousemove', function(evento){
+    let rect = canvas1.getBoundingClientRect()
+    let x_mouse = evento.clientX - rect.left;
+    let y_mouse = evento.clientY - rect.top;
+    console.log(x_mouse,y_mouse)
+
+    retangulo3.x = x_mouse;
+    retangulo3.y = y_mouse;
+
+})
+
