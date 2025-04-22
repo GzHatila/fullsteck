be9aadd41c0cc7db7cd6311a0a5c93b7c3f6adac
GzHatila <hatila07112006@gmail.com>
2025-04-22 15:27:52 -0300

    .

--- Servidor/aula01.js
+++ Servidor/aula01.js
@@ -8,6 +8,12 @@ app.use(express.static('./public'));
 var server = http.createServer(app);
 server.listen(80);
 
+let bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 console.log("Servidor Rodando".rainbow);    
 
@@ -15,4 +21,28 @@ console.log("Servidor Rodando".rainbow);
 
 app.get("/inicio", function(requisicao, resposta){
     resposta.redirect("LAB/index.html");
+})
+
+app.post("/inicio", function(requisicao, resposta){
+    resposta.redirect("LAB/index.html");
+})
+
+app.get("/cadastrar", function(requisicao, resposta){
+    let Nome = requisicao.query.Nome;
+    let Login = requisicao.query.Login;
+    let Senha = requisicao.query.Senha;
+    let Nasc = requisicao.query.nascimento;
+
+    console.log(Nome,Login,Senha,Nasc)
+})
+
+app.post("/cadastrar", function(requisicao,resposta){
+    let Nome = requisicao.body.Nome;
+    let Login = requisicao.body.Login;
+    let Senha = requisicao.body.Senha;
+    let Nasc = requisicao.body.nascimento;
+
+    console.log(Nome,Login,Senha,Nasc);
+
+    resposta.render("resposta",{Nome,Login,Senha,Nasc});
 })
